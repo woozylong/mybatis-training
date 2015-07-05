b7487c4f11d6bff6f1f43425f389c7333625e6c6
woozylong <woozylong@gmail.com>
2015-07-05 16:44:51 +0800

    懒加载

--- src/test/java/mybatis/Test.java
+++ src/test/java/mybatis/Test.java
@@ -31,10 +31,18 @@ public class Test {
 	@org.junit.Test
 	public void testManytoOne(){
 		SqlSession session = sessionFactory.openSession();
+		System.out.println("step 1:");
 		User user = session.selectOne("com.yonyou.mapping.UserMapper.getUserAsso", 1);
+		System.out.println("step 2:");
 		System.out.println(user.getGroup());
+		session.close();
+		System.out.println("===============================");
+		//嵌套select方式，采用了懒加载，要在conf.xml setting配置
+		session = sessionFactory.openSession();
+		System.out.println("step 1:");
 		//namespace和接口类名一致，这样可以使用getmapper
 		UserMapper userMapper = session.getMapper(UserMapper.class);
+		System.out.println("step 2:");
 		user = userMapper.getUserAsso(1);
 		System.out.println("usermapper:" + user.getGroup());
 		session.close();
